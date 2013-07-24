d9ac861f5fc378f1d9115b617c4c09be85e1b0ce
Humberto Oliveros <holiveros@gmail.com>
2013-07-24 06:03:37 +0000

    *Add some content for a possible idea

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+
 app.get('/', function(request, response) {
   var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8");
   response.send(buffer.toString());
